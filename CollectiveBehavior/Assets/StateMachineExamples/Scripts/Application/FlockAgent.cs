ef6255b2ac9014d18ba9641b6b40e04b2f27bbd9
Ziming He <33373110+zimhe@users.noreply.github.com>
2018-11-25 22:48:28 +0000

    Collective

---
+++ CollectiveBehavior/Assets/StateMachineExamples/Scripts/Application/FlockAgent.cs
@@ -0,0 +1,187 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockAgent : MonoBehaviour
+{
+    float direction = 1f;
+    float _speed = 10f;
+
+    Vector3 toAvoid = Vector3.zero;
+
+    public Color lookColor = Color.green;
+
+    List<Transform> _sameType;
+
+    bool flocking;
+    public bool IsFlocking()
+    {
+        return flocking;
+    }
+
+    public float GetSpeed()
+    {
+        return _speed;
+    }
+    public float GetDirection()
+    {
+        return direction;
+    }
+
+    private void Start()
+    {
+        _sameType = new List<Transform>();
+    }
+
+
+    BlockTypes _type;
+
+    public void SetType(int t)
+    {
+        switch (t)
+        {
+            case 0:
+                _type = BlockTypes.Type_A;
+
+                GetComponent<MeshRenderer>().material.color = Color.red;
+                break;
+            case 1:
+                _type = BlockTypes.Type_B;
+                GetComponent<MeshRenderer>().material.color = Color.blue;
+                break;
+            case 2:
+                _type = BlockTypes.Type_C;
+                GetComponent<MeshRenderer>().material.color = Color.green;
+                break;
+        }
+    }
+
+    public BlockTypes GetType()
+    {
+        return _type;
+    }
+
+    public  void UpdateFlock(FlockManager manager)
+    {
+        int flag = 0;
+      
+        transform.localPosition += transform.forward * _speed * direction * Time.deltaTime;
+        //check obstacle in forward 
+        LookAt(transform.forward, transform.localScale.z + manager._lookRange);
+        //check obstacle in back 
+        LookAt(-transform.forward, transform.localScale.z + manager._lookRange);
+        //check obstacle in right 
+        LookAt(transform.right, transform.localScale.x + manager._lookRange);
+        //check obstacle in left 
+        LookAt(-transform.right, transform.localScale.x + manager._lookRange);
+
+        if (Random.Range(0, 100) < manager._updateRate)
+        {
+            Flocking(manager);
+            ChangeSpeed(manager);
+        }
+    }
+
+
+    void LookAt(Vector3 dir,float range)
+    {
+       
+       
+        Debug.DrawRay(transform.position, dir*range,lookColor);
+
+        RaycastHit hit;
+
+        bool b = Physics.Raycast(transform.position,dir,out hit,range);
+
+        if (b == true)
+        {
+            if (hit.transform.CompareTag("Objects")&&hit.transform!=transform)
+            {
+                var ai = hit.transform.GetComponent<FlockAgent>();
+
+                if (ai && ai.GetType() == _type)
+                {
+                    return;
+                }
+                else
+                {
+                    toAvoid = hit.point;
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="manager"></param>
+    void Flocking(FlockManager manager)
+    {
+        Vector3 centre = Vector3.zero;
+        Vector3 avoid = Vector3.zero;
+        float fSpeed = 0.01f;
+        float nDistance;
+        int groupSize = 0;
+        foreach (var a in manager.GetAllFlock())
+        {
+            if (a == transform)
+            {
+                continue;
+            }
+            var agt = a.GetComponent<FlockAgent>();
+
+            nDistance = Vector3.Distance(a.transform.position, transform.position);
+            if (nDistance <= manager._neighborDistance)
+            {
+                if (agt.GetType() == _type)
+                {
+                    centre += a.transform.position;
+                }
+                else
+                {
+                    centre += (transform.position - a.position);
+                }
+                groupSize++;
+
+                if (nDistance < manager._avoidDistance)
+                {
+                    avoid += (transform.position - a.position);
+                }
+
+                fSpeed += agt.GetSpeed();
+            }
+        }
+        if (groupSize > 0)
+        {
+            centre = (centre) / groupSize+(manager.GetGoalPos(this));
+            _speed = fSpeed / groupSize;
+
+            Vector3 dir = (centre + avoid) - transform.position+(-toAvoid);
+
+            if (dir != Vector3.zero)
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), manager._flockSpeed * Time.deltaTime);
+                //if (fDirection != 0)
+                //    direction = fDirection / Mathf.Abs(fDirection);
+
+                flocking = true;
+                Debug.DrawRay(transform.position, dir.normalized * manager._lookRange * 2f, Color.red);
+            }
+        }
+        else
+        {
+            flocking = false;
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(-toAvoid), manager._flockSpeed * Time.deltaTime);
+        }
+    }
+    /// <summary>
+    /// change Speed by probability
+    /// </summary>
+    void ChangeSpeed(FlockManager manager)
+    {
+        if (Random.Range(0, 100) < 20)
+        {
+            _speed = Random.Range(manager._minSpeed, manager._maxSpeed);
+        }
+    }
+}
---
+++ CollectiveBehavior/Assets/StateMachineExamples/Scripts/Application/FlockManager.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using UnityEngine;
+
+public class FlockManager : MonoBehaviour
+{
+    [SerializeField] private Transform wallPrefab;
+    [SerializeField] private float _rangeX = 10f;
+    [SerializeField] private float _rangeY = 10f;
+    [SerializeField] private float _rangeZ = 10f;
+
+    [SerializeField] private int _flockCount = 100;
+
+    [Header( " FlockParameter")]
+    public float _maxSpeed = 20f;
+    public float _minSpeed = 3f;
+    public float _lookRange = 5f;
+    public float _neighborDistance = 20f;
+    public float _avoidDistance = 5f;
+    public float _flockSpeed = 1f;
+    public float _updateRate = 50f;
+
+    bool _playing = true;
+
+    private List<Vector3> _savedPositions;
+
+    private Dictionary<Transform, Vector3> _positionDic;
+    private Dictionary<Transform, Quaternion> _rotationDic;
+
+    private List<Transform> _flock;
+
+    public void SwitchPlay()
+    {
+        _playing = !_playing;
+    }
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        CreateFlock();
+    }
+
+    void CreateFlock()
+    {
+        float sizeX = wallPrefab.localScale.x;
+        float sizeZ = wallPrefab.localScale.z;
+
+        _savedPositions = new List<Vector3>(_flockCount);
+        _flock = new List<Transform>(_flockCount);
+        _positionDic = new Dictionary<Transform, Vector3>();
+        _rotationDic = new Dictionary<Transform, Quaternion>();
+
+        for (int i = 0; i < _flockCount; i++)
+        {
+
+            var x = UnityEngine.Random.Range(-_rangeX, _rangeX);
+            var y = UnityEngine.Random.Range(-_rangeY, _rangeY);
+            var z = UnityEngine.Random.Range(-_rangeZ, _rangeZ);
+
+            Vector3 p = new Vector3(x,y,z);
+
+                _savedPositions.Add(p);
+
+        }
+
+        foreach (var pos in _savedPositions)
+        {
+            var w = Instantiate(wallPrefab, transform);
+            w.localPosition = pos;
+            _flock.Add(w);
+        }
+
+        foreach (var w in _flock)
+        {
+            if (w.GetComponent<FlockAgent>())
+            {
+                int t = UnityEngine.Random.Range(0, 3);
+                w.GetComponent<FlockAgent>().SetType(t);
+            }
+            _positionDic.Add(w, w.localPosition);
+            _rotationDic.Add(w, w.localRotation);
+        }
+    }
+
+    public List<Transform> GetAllFlock()
+    {
+        return _flock;
+    }
+
+   public Vector3 GetGoalPos(FlockAgent agent)
+    {
+        Vector3 gol = Vector3.zero;
+
+        foreach(var a in _flock)
+        {
+            var ai = a.GetComponent<FlockAgent>();
+
+            if (ai.IsFlocking()||ai.GetType()==agent.GetType())
+            {
+                continue;
+            }
+            else
+            {
+                gol = a.position;
+
+                break;
+            }
+
+        }
+        if (gol == Vector3.zero)
+        {
+            return gol;
+        }
+        else
+        {
+            return gol - agent.transform.position;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_playing)
+        {
+            foreach (var a in _flock)
+            {
+                var ai = a.GetComponent<FlockAgent>();
+
+                ai.UpdateFlock(this);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SwitchPlay();
+        }
+     
+    }
+}
